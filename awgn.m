d0a05660e9cf848f25910801e95afce6b1e0592f
NicolasOrtega <42541336+NicolasOrtega@users.noreply.github.com>
2018-09-06 12:37:30 -0300

    Create awgn.m

---
+++ awgn.m
@@ -0,0 +1,8 @@
+function [n_I, n_Q, EstadoAWGN_I, EstadoAWGN_Q] = ...
+    awgn(EbNo, L, EstadoAWGN_I, EstadoAWGN_Q, M)
+No = 1/(10^(EbNo/10));
+[n_I,EstadoAWGN_I] = randg(L,EstadoAWGN_I);
+n_I = sqrt(No/2)*n_I;
+[n_Q,EstadoAWGN_Q] = randg(L,EstadoAWGN_Q);
+n_Q = sqrt(No/2)*n_Q;
+end
