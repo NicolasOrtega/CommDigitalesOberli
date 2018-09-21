85820234f4fa1583f044c45b57afe93a31c162fe
NicolasOrtega <42541336+NicolasOrtega@users.noreply.github.com>
2018-09-21 13:24:03 -0300

    Update awgn.m

--- awgn.m
+++ awgn.m
@@ -1,6 +1,7 @@
 function [n_I, n_Q, EstadoAWGN_I, EstadoAWGN_Q] = ...
     awgn(EbNo, L, EstadoAWGN_I, EstadoAWGN_Q, M)
-No = 1/(10^(EbNo/10));
+Eb = 1/log2(M);
+No = Eb/(10^(EbNo/10));
 [n_I,EstadoAWGN_I] = randg(L,EstadoAWGN_I);
 n_I = sqrt(No/2)*n_I;
 [n_Q,EstadoAWGN_Q] = randg(L,EstadoAWGN_Q);
