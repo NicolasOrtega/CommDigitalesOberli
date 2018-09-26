25a662424ad68d6af64037401f7f0eedc9ac881c
NicolasOrtega <42541336+NicolasOrtega@users.noreply.github.com>
2018-09-26 16:51:44 -0300

    Update errores.m

--- errores.m
+++ errores.m
@@ -1,10 +1,10 @@
 function [Err_s,Err_b] = errores(MensajeBinario,MensajeBinarioRx,M)
 Err_b = length(nonzeros(xor(MensajeBinario,MensajeBinarioRx)));
-symTx = reshape(MensajeBinario,length(MensajeBinarioRx)/log2(M),log2(M));
-symRx = reshape(MensajeBinarioRx,length(MensajeBinarioRx)/log2(M),log2(M));
+symTx = reshape(MensajeBinario,log2(M),length(MensajeBinarioRx)/log2(M));
+symRx = reshape(MensajeBinarioRx,log2(M),length(MensajeBinarioRx)/log2(M));
 errs = 0;
-for i = 1:size(symTx,1)
-    if isequal(symTx(i,:),symRx(i,:))==0
+for i = 1:size(symTx,2)
+    if isequal(symTx(:,i),symRx(:,i))==0
         errs = errs+1;
     end
 end
